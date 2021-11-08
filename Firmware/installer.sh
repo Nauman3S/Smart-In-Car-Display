063294517583acd044a2a364f7daa527508c1f93
Nauman3S <naumanshakir3s@gmail.com>
2021-11-08 10:10:54 +0500

    added .gitignore

--- Firmware/installer.sh
+++ Firmware/installer.sh
@@ -1,10 +1,15 @@
 sudo apt install whiptail -y
 
 phases=( 
-    'Creating directories...'
-    'Installing System Packages...'
-    'Installing python packages...'
-    'Finalizing installation...'
+    '1) Creating directories, Downloading Firmware...'
+    '2) Installing System Packages...'
+    '3) Installing python packages...'
+    '4) Downloading and Installing HyperPixel...'
+    '5) Setting up Scripts...'
+    '6) Configuring Startup Script...'
+    '7) Finalizing Installation...'
+    '8) Finalizing Installation...'
+    
 )   
 
 for i in $(seq 1 100); do  
@@ -12,7 +17,7 @@ for i in $(seq 1 100); do
 
     if [ $i -eq 100 ]; then
         echo -e "XXX\n100\nDone!\nXXX"
-    elif [ $(($i % 25)) -eq 0 ]; then
+    elif [ $(($i % 15)) -eq 0 ]; then
         let "phase = $i / 25"
         echo -e "XXX\n$i\n${phases[phase]}\nXXX"
     else
@@ -27,18 +32,18 @@ for i in $(seq 1 100); do
             echo "Error: Directory SmartInCarDisplay does not exists."
             cd $HOME
             # mkdir ~/RPiClient
-            git clone \
-            --depth 1  \
-            --filter=blob:none  \
-            --sparse \
-            https://github.com/Nauman3S/Smart-In-Car-Display.git;
-            cd Smart-In-Car-Display
+            git clone https://github.com/Nauman3S/Smart-In-Car-Display.git;
+            cd Smart-In-Car-Display;
             git sparse-checkout set Firmware
-            mv Firmware ../
+            mkdir SmartInCarDisplay
+            mkdir SmartInCarDisplay/Firmware
+            mkdir SmartInCarDisplay/temp
+            mv Firmware ../SmartInCarDisplay/Firmware
             cd ..
             rm -rf Smart-In-Car-Display
-            mkdir SmartInCarDisplay
-            mv Firmware SmartInCarDisplay
+            cd SmartInCarDisplay/Firmware
+            sudo chmod a+rx starter.sh
+            clear;
         fi
         if [ -d "$HOME/SmartInCarDisplay/logs" ]
         then
@@ -48,7 +53,7 @@ for i in $(seq 1 100); do
         mkdir ~/SmartInCarDisplay/logs
         fi
     elif [ $i -eq 10 ]; then
-        sudo apt-get update
+        sudo apt-get update &>/dev/null
         clear
     elif [ $i -eq 15 ]; then
         sudo apt install python3-pip
@@ -57,11 +62,18 @@ for i in $(seq 1 100); do
     elif [ $i -eq 25 ]; then
         sudo pip3 install pillow
     elif [ $i -eq 30 ]; then
-        sudo pip3 install python3-dev
+        sudo pip3 install python3-dev &>/dev/null
+        sudo apt install libpython3-dev
     elif [ $i -eq 35 ]; then
         sudo pip3 install hyperpixel2r
     elif [ $i -eq 40 ]; then
         sudo pip3 install Adafruit_DHT
+    elif [ $i -eq 50 ]; then
+        cd $HOME
+        cd SmartInCarDisplay/temp
+        git clone https://github.com/pimoroni/hyperpixel2r
+        cd hyperpixel2r
+        sudo ./install.sh
     fi
 done | whiptail --title 'Smart In-Car Display Installer' --gauge "${phases[0]}" 8 70 0
 
