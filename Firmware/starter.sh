9f8743a9c4bd28d3e48c13bbff2a7d1bbbe63865
Nauman3S <naumanshakir3s@gmail.com>
2021-12-11 09:06:44 +0500

    updated

--- Firmware/starter.sh
+++ Firmware/starter.sh
@@ -1,3 +1,9 @@
-killall -9 python3
-sudo killall -9 python3
-python3 Firmware.py
+
+#!/bin/bash
+#killall -9 python3
+#sudo killall -9 python3
+#python3 Firmware.py
+
+sleep 5
+(sudo /usr/bin/python3 /home/pi/SmartInCarDisplay/Firmware/Firmware.py >/home/pi/SmartInCarDisplay/Firmware/main_logs.txt 2>&1)
+
